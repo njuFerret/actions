85ba5c35fa6f328256ca828ed387f896737d0112
njuFerret <emagazi.cn@gmail.com>
2024-10-21 08:43:51 +0800

    init workflow

---
+++ version.py
@@ -0,0 +1,13 @@
+import pathlib
+
+root = pathlib.Path(__file__).parent
+version_file = root / "version.txt"
+version_folder = root.joinpath("versions")
+version_folder.mkdir(exist_ok=True, parents=True)
+
+for l in version_file.open("r", encoding='utf-8').readlines():
+    lib, version = l.strip().split("=")
+    lib = lib.strip()
+    version = version.strip()
+    with version_folder.joinpath(f"{lib}").open("w", encoding='utf-8') as f:
+        f.write(f'{version}')
